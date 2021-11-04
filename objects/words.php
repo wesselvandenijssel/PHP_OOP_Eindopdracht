8de9b5e5a75ccb3edf5c0e4ebcfa18ca0bc4f9f8
Wessel van den Ijssel <wessel069@gmail.com>
2021-11-04 13:21:37 +0100

    read + edit

--- objects/words.php
+++ objects/words.php
@@ -22,18 +22,62 @@ class Words
        return $result;
    }
    function create(){
-    // select all query
-   $query = "UPDATE * FROM " . $this->table_name;
-   $result = $this->conn->query($query);
-   return $result;
-   echo $result;
+   if (isset($_POST["create"])){
+    $word= $_POST['word'];
+    $checked = $_POST['checked'];
+    $grace = $_POST['grace'];
+    
+    $sql = "INSERT INTO $this->table_name (word, checked, grace) VALUES ('$word','$checked','$grace')";
+    $result = mysqli_query($this->conn, $sql);
+    // echo " last toegevoegd " . mysqli_insert_id($conn);
+    if($result)
+    if($result){
+    
+    }
+    else
+    {
+        echo "Error :".$sql;
+    }
+    }
 }
+function readUpdate()
+   {
+    if (isset($_GET["id"])){
+        $id = $_GET["id"];
+       $query = "SELECT * FROM " . $this->table_name . " WHERE id = $id";
+    //    echo $query;
+       }
+       else{
+        $query = "SELECT * FROM " . $this->table_name;
+        // echo $query;
+       }
+       $result = $this->conn->query($query);
+       return $result;
+   }
 function update(){
     // select all query
-   $query = "UPDATE * FROM " . $this->table_name;
-   $result = $this->conn->query($query);
-   return $result;
-   echo $result;
+    if(isset($_POST['update'])){
+        $id = $_GET["update"];
+        $word= $_POST['word'];
+        $checked = $_POST['checked'];
+        $grace = $_POST['grace'];
+    
+        $sql = "UPDATE `$this->table_name` SET `word`='$word',`checked`='$checked',
+        `grace`='$grace' WHERE `id` = '$id'";
+        // echo $sql;
+        // echo $id;
+        // echo $title;
+        $result = mysqli_query($this->conn, $sql);
+        if($result){
+             echo '<script type="text/javascript"> alert("Data Update")</script>';
+             header ("refresh:0;url=read_all.php");
+             echo "Redirecting.....";
+    
+        }
+        else{
+            echo '<script type="text/javascript"> alert("Data NOT Update")</script>';
+        }
+    }
 }
 function delete(){
     // select all query
